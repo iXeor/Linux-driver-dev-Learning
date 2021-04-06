e19a3b32dca01a7f9dd418374b3645ca01348ad2
Akatsuki <43687278+iXeor@users.noreply.github.com>
2021-04-06 17:29:13 +0800

    Add files via upload

---
+++ 4/VDD.mod.c
@@ -0,0 +1,41 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+ .name = KBUILD_MODNAME,
+ .init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+ .exit = cleanup_module,
+#endif
+ .arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x44f187c3, "module_layout" },
+	{ 0x2db24e39, "cdev_del" },
+	{ 0x2c38e8fe, "kmalloc_caches" },
+	{ 0xd2920a26, "cdev_init" },
+	{ 0xd8e484f0, "register_chrdev_region" },
+	{ 0x105e2727, "__tracepoint_kmalloc" },
+	{ 0x7485e15e, "unregister_chrdev_region" },
+	{ 0x8a33d0e4, "kmem_cache_alloc_notrace" },
+	{ 0xb72397d5, "printk" },
+	{ 0xb4390f9a, "mcount" },
+	{ 0xeaa9e6a6, "cdev_add" },
+	{ 0x37a0cba, "kfree" },
+	{ 0x29537c9e, "alloc_chrdev_region" },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "CCE12A38E60D63E4DC4F001");

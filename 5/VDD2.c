3c22cf098cd4dfb272640f17d654cd9cba397ab7
Akatsuki <43687278+iXeor@users.noreply.github.com>
2021-04-06 17:31:36 +0800

    Add files via upload

---
+++ 5/VDD2.c
@@ -0,0 +1,214 @@
+#define VIRTUALDISK_SIZE 0xFFFF
+#define VIRTUALDISK_NUM 200
+#define MEM_CLEAR 0x1
+#define PORT1_SET 0x2
+#define PORT2_SET 0x3
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/cdev.h>
+#include <linux/sched.h>
+#include <linux/mm.h>
+#include <asm/system.h>
+#include <asm/uaccess.h>
+#include <asm/io.h>
+
+typedef struct VirtualDisk
+{
+	struct cdev cdev;
+	unsigned char memory[VIRTUALDISK_SIZE];
+	int port1;
+	long port2;
+	long count;
+} vdDriver;
+
+vdDriver *VirtualDisk_devp;
+
+int Virtualdisk_open(struct inode *inode,struct file *fp)
+{
+	vdDriver *devp = fp->private_data;
+	fp->private_data = VirtualDisk_devp;	
+	devp->count++;
+	return 0;
+}
+int Virtualdisk_release(struct inode *inode,struct file *fp)
+{
+	vdDriver *devp = fp->private_data;
+	devp->count--;
+	return 0;
+}
+static ssize_t Virtualdisk_read(struct file *fp,char __user *buff,size_t size,loff_t *ppos)
+{
+	unsigned long p = *ppos;
+	unsigned int count = size;
+	int result = 0;
+	vdDriver *devp = fp->private_data;
+	if(p>=VIRTUALDISK_SIZE)
+	{
+		return count ? - ENXIO:0;
+	}
+	if(count > VIRTUALDISK_SIZE - p)
+	{
+		count = VIRTUALDISK_SIZE - p;
+	}
+	if(copy_to_user(buff,(void*)(devp->memory+p),count))
+	{
+		result = -EFAULT;
+	}
+	else
+	{
+		*ppos += count;
+		result = count;
+		printk(KERN_INFO"Read %u byte(s) from %lu\n",count,p);
+	}
+	return result;
+}
+static ssize_t Virtualdisk_write(struct file *fp,const char __user *buff,size_t size,loff_t *ppos)
+{
+	unsigned long p = *ppos;
+	unsigned int count = size;
+	int result = 0;
+	vdDriver *devp = fp->private_data;
+	if(p>=VIRTUALDISK_SIZE)
+	{
+		return count ? - ENXIO:0;
+	}
+	if(count > VIRTUALDISK_SIZE - p)
+	{
+		count = VIRTUALDISK_SIZE - p;
+	}
+	if(copy_from_user((void*)(devp->memory+p),buff,count))
+	{
+		result = -EFAULT;
+	}
+	else
+	{
+		*ppos += count;
+		result = count;
+		printk(KERN_INFO"Written %u byte(s) from %lu\n",count,p);
+	}
+	return result;
+}
+static loff_t Virtualdisk_llseek(struct file *fp,loff_t offset,int o)
+{
+	loff_t result = 0;
+	switch(o)
+	{
+		case SEEK_SET:
+			if(offset < 0)
+			{
+				result = -EINVAL;
+				break;
+			}
+			if((unsigned int)offset > VIRTUALDISK_SIZE)
+			{
+				result = -EINVAL;
+				break;
+			}
+			fp->f_pos = (unsigned int)offset;
+			result = fp->f_pos;
+			break;
+		case SEEK_CUR:
+			if((fp->f_pos + offset)>VIRTUALDISK_SIZE)
+			{
+				result = -EINVAL;
+				break;
+			}
+			if((fp->f_pos + offset)<0)
+			{
+				result = -EINVAL;
+				break;
+			}
+			fp->f_pos += offset;
+			result = fp->f_pos;
+			break;
+		default:
+			result = -EINVAL;
+			break;
+	}
+	return result;
+}
+static int Virtualdisk_ioctl(struct inode *inodep,struct file *fp,unsigned int cmd,unsigned long arg)
+{
+	vdDriver *devp = fp->private_data;
+	switch(cmd)
+	{
+		case MEM_CLEAR:
+			memset(devp->memory,0,VIRTUALDISK_SIZE);
+			printk(KERN_INFO"Clear Succeed\n");
+			break;
+		case PORT1_SET:
+			devp->port1 = 0;
+			break;
+		case PORT2_SET:
+			devp->port2 = 0;
+			break;
+		default:
+			return -EINVAL;
+			break;
+	}
+	return 0;
+}
+
+static const struct file_operations vdd_fops = {
+	.owner = THIS_MODULE,
+	.llseek = Virtualdisk_llseek,
+	.read = Virtualdisk_read,
+	.write = Virtualdisk_write,
+	.ioctl = Virtualdisk_ioctl,
+	.open = Virtualdisk_open,
+	.release = Virtualdisk_release,
+};
+
+static int majorno = VIRTUALDISK_NUM;
+
+static void setup_cdev(vdDriver *devp,dev_t devno)
+{
+	int result;
+	cdev_init(&devp->cdev,&vdd_fops);
+	devp->cdev.owner = THIS_MODULE;
+	devp->cdev.ops = &vdd_fops;
+	result = cdev_add(&devp->cdev,devno,1);
+	if (result<0)
+		printk(KERN_NOTICE"ERROR IN ADD cdev!\n");
+}
+static int VirtualDisk_init(void)
+{
+	int result;
+	dev_t devno = MKDEV(majorno,0);
+	if(majorno)
+	{
+		result = register_chrdev_region(devno,1,"VirtualDisk");
+	}
+	else 
+	{
+		result = alloc_chrdev_region(&devno,0,1,"VirtualDisk");
+		majorno = MAJOR(devno);
+	}
+	if(result<0) return result;
+	VirtualDisk_devp = kmalloc(sizeof(vdDriver),GFP_KERNEL);
+	if(!VirtualDisk_devp)
+	{
+		result = -ENOMEM;
+		goto fail_malloc;
+	}
+	memset(VirtualDisk_devp,0,sizeof(vdDriver));
+	setup_cdev(VirtualDisk_devp,devno);
+	printk("Device Regist Successed! devno=%d\n",devno);
+fail_malloc:
+	unregister_chrdev_region(devno,1);
+	return result;
+}
+static void VirtualDisk_exit(void)
+{
+	cdev_del(&VirtualDisk_devp->cdev);
+	kfree(VirtualDisk_devp);
+	unregister_chrdev_region(MKDEV(majorno,0),1);
+	printk("Device Unregist Succeed!\n");
+}
+module_init(VirtualDisk_init);
+module_exit(VirtualDisk_exit);
+MODULE_LICENSE("Dual BSD/GPL");
+
---
+++ 5/VDD2.mod.c
@@ -0,0 +1,43 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+ .name = KBUILD_MODNAME,
+ .init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+ .exit = cleanup_module,
+#endif
+ .arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x44f187c3, "module_layout" },
+	{ 0x2db24e39, "cdev_del" },
+	{ 0xd2920a26, "cdev_init" },
+	{ 0xd8e484f0, "register_chrdev_region" },
+	{ 0x105e2727, "__tracepoint_kmalloc" },
+	{ 0x7485e15e, "unregister_chrdev_region" },
+	{ 0x2bc95bd4, "memset" },
+	{ 0xb72397d5, "printk" },
+	{ 0x2f287f0d, "copy_to_user" },
+	{ 0xb4390f9a, "mcount" },
+	{ 0xeaa9e6a6, "cdev_add" },
+	{ 0x93fca811, "__get_free_pages" },
+	{ 0x37a0cba, "kfree" },
+	{ 0x362ef408, "_copy_from_user" },
+	{ 0x29537c9e, "alloc_chrdev_region" },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "816E28A47EE2DE12A5BD91E");

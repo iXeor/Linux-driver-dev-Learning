5ae365d8b8fde7e6c52ac6b7d8cf75ae683ed977
Akatsuki <43687278+iXeor@users.noreply.github.com>
2021-04-06 19:01:37 +0800

    Update 2.test.c

--- Annotated source code/2.test.c
+++ Annotated source code/2.test.c
@@ -1,4 +1,4 @@
-//Linux内核驱动开发代码2：模块之间的通信。教材第84、85页5.3.3
+//Linux内核驱动开发代码2：模块之间的通信。
 /*******************************************************/
 #include <linux/init.h>		   //Linux内核中定义了初始化数据相关的宏的头文件（module_init和moudle_exit的定义在这里）
 #include <linux/module.h>	   //Linux内核中定义了模块相关处理的头文件，用于动态加载模块至内核（Dynamic loading of modules into the kernel.）
